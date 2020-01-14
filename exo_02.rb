261b2b76b68b03e58a9ddc045dcad5a1355a088e
thebidixhd <bidiboy012@gmail.com>
2020-01-14 16:32:15 +0100

    add new files

---
+++ exo_02.rb
@@ -0,0 +1,4 @@
+puts "Bonjour, monde !
+Et avec une voix sexy, ça donne : Bonjour, monde !"
+
+#La méthode puts ajoute automatiquement une nouvelle ligne à la fin de ton message, à chaque fois que tu l’utilise Si tu ne souhaites pas avoir cette fameuse nouvelle ligne, à la fin de chaque exécution, tu devras utiliser la méthode print.#
---
+++ exo_03.rb
@@ -0,0 +1,4 @@
+puts "Bonjour, monde !
+#{Et avec une voix sexy, ça donne : Bonjour, monde !}
+
+"#ca me met un message d'erreur"
